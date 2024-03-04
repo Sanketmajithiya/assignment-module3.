63b53bc3fbaba15588b926f0376b250df50262e8
Sanketmajithiya <143784345+Sanketmajithiya@users.noreply.github.com>
2024-03-04 14:58:02 +0530

    Update Q14.py

--- Q14.py
+++ Q14.py
@@ -1,7 +1,5 @@
 # Q.14 Write a Python program to find the second smallest number in a list. 
 
-
-def second_smalllest(numbers):
-    return sorted(numbers)[1]
-my_list = [8,4,5,6,3,1]
-print ("second smalllest number:", second_smalllest(my_list))
+my_list = [5,10,20,30,50]
+new_list = sorted(my_list)
+print = (new_list)
